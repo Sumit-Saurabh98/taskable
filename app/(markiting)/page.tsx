3f26e771eae9400a49f2f4725269e9b93e6af718
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2024-05-16 09:02:39 +0530

    done

---
+++ app/(markiting)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const MarketingPage = () => {
+  return (
+    <>
+      <h1 className="text-red-500">Hello next js</h1>
+      <Button className="bg-emerald-500 text-white">Cick me</Button>
+    </>
+  );
+}
+
+export default MarketingPage;
